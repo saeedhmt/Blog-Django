ec99f9e8da1d3827e0db8a3dadb544140459fa44
saeedhmt <saeed.hemati10894@gmail.com>
2021-02-02 17:32:43 +0330

    base and index html modified, added url and view for new post, also created html file for new post.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
+    path('newpost/', views.new_post, name='newpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
+from django.urls import reverse
+
+from blog.forms import PostModelForm
 from blog.models import Post
 from django.views import generic
 
@@ -16,4 +20,23 @@ class IndexListView(generic.ListView):
     context_object_name = 'latest_posts'
 
     def get_queryset(self):
-        return Post.objects.filter(show=True).order_by('-datetime')[:5]
+        return Post.objects.filter(active=True).filter(show=True).order_by('-datetime')[:5]
+
+@login_required
+@permission_required('blog.add_post')
+def new_post(request):
+    if request.method == 'POST':
+        form_post = PostModelForm(request.POST)
+
+        if form_post.is_valid():
+            post = form_post.save(commit=False)
+            post.author = request.user
+            post.save()
+            form_post._save_m2m()
+
+            return HttpResponseRedirect(reverse('blog:index'))
+
+    else:
+        form_post = PostModelForm()
+
+    return render(request, 'blog/newpost.html', {'form_post' : form_post})
